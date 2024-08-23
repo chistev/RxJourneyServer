080dfd85b2018600eb608f05b9562c1456e7bb09
chistev <stephenowabie@gmail.com>
2024-08-24 00:14:27 +0100

    modified the views so that only the get random posts view remains

--- detail/urls.py
+++ detail/urls.py
@@ -1,11 +1,6 @@
 from django.urls import path
-from .views import toggle_like, add_comment, get_comments, delete_comment, edit_comment, random_posts
+from .views import random_posts
 
 urlpatterns = [
-    path('toggle-like/<int:post_id>/', toggle_like, name='toggle-like'),
-    path('add-comment/', add_comment, name='add_comment'),
-    path('comments/<int:post_id>/', get_comments, name='get_comments'),
-    path('delete-comment/<int:comment_id>/', delete_comment, name='delete_comment'),
-    path('edit-comment/<int:comment_id>/', edit_comment, name='edit_comment'),
     path('random-posts/<slug:slug>/', random_posts, name='random-posts'),
 ]
