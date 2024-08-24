e9746c769b706aab09cf36cc6521f8f3d9a1dc03
chistev <stephenowabie@gmail.com>
2024-08-24 22:23:50 +0100

    notifications getting sent for new posts to all subscribers; html escaped

---
+++ home/email_services.py
@@ -0,0 +1,57 @@
+import json
+import os
+import requests
+
+def send_post_notification(subject, post_title, post_excerpt, post_slug):
+    from .models import Subscriber
+
+    api_key = os.environ.get('BREVO_API_KEY')
+    if not api_key:
+        raise ValueError("API key not found. Please set the 'BREVO_API_KEY' environment variable.")
+
+    api_url = 'https://api.brevo.com/v3/smtp/email'
+    sender_email = 'stephenowabie@gmail.com'
+    sender_name = 'Chistev'
+
+    subscribers = Subscriber.objects.all()
+    recipient_emails = [subscriber.email for subscriber in subscribers]
+
+    for email in recipient_emails:
+        message = f"""
+        <html>
+        <body>
+            <p>Hello,</p>
+            <p>We have a new post on our blog!<br><br>
+            <strong>{post_title}</strong></p>
+            <p>{post_excerpt}</p>
+            <p><a href="http://localhost:5173/{post_slug}">Read more</a></p>
+        </body>
+        </html>
+        """
+
+        payload = {
+            "sender": {
+                "name": sender_name,
+                "email": sender_email,
+            },
+            "to": [
+                {
+                    "email": email
+                }
+            ],
+            "subject": subject,
+            "htmlContent": message
+        }
+
+        headers = {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'api-key': api_key
+        }
+
+        response = requests.post(api_url, data=json.dumps(payload), headers=headers)
+
+        if response.status_code == 201:
+            print(f"Notification email sent successfully to {email}.")
+        else:
+            print(f"Failed to send notification email to {email}. Response: {response.text}")
--- home/models.py
+++ home/models.py
@@ -1,6 +1,9 @@
 from django.db import models
+from django.utils.html import strip_tags
 from django.utils.text import slugify
 from django_ckeditor_5.fields import CKEditor5Field
+from home.email_services import send_post_notification
+import re
 
 
 class Post(models.Model):
@@ -10,10 +13,30 @@ class Post(models.Model):
     content = CKEditor5Field('Text', config_name='extends')
     created_at = models.DateTimeField(auto_now_add=True)
 
+    def get_excerpt(self, word_limit=100):
+        # Strip HTML tags from content
+        plain_text_content = strip_tags(self.content)
+
+        # Replace HTML entities like &nbsp; with a space
+        plain_text_content = plain_text_content.replace('&nbsp;', ' ')
+
+        # Split the content into words
+        words = re.findall(r'\b\w+\b', plain_text_content)
+
+        # Limit the number of words
+        truncated_words = words[:word_limit]
+
+        # Join words back into a string and add ellipsis if truncated
+        truncated_content = ' '.join(truncated_words)
+        return f"{truncated_content}..." if len(words) > word_limit else truncated_content
+
     def save(self, *args, **kwargs):
-        if not self.slug:
-            self.slug = slugify(self.title)
+        is_new = self.pk is None
         super().save(*args, **kwargs)
+        if is_new:
+            post_excerpt = self.get_excerpt()
+            subject = f"New Post: {self.title}"
+            send_post_notification(subject, self.title, post_excerpt, self.slug)
 
     def __str__(self):
         return self.title
