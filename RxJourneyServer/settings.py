5c9e2d928ce407ea28a9c07ab200589b5bb34986
chistev <stephenowabie@gmail.com>
2024-08-31 14:07:21 +0000

    first push from pythonanywhere

--- RxJourneyServer/settings.py
+++ RxJourneyServer/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = env('SECRET_KEY')
 # DEBUG = True
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['rxjourneyserver.pythonanywhere.com']
 
 
 # Application definition
@@ -62,13 +62,20 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+# Allowed origins for CORS
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:5173',
+    'http://localhost:5173',  # Local development
+    'https://rxjourney.onrender.com',  # Production
+    'https://rxjourney.com.ng',  # Production
+    'https://www.rxjourney.com.ng',  # Production with www
 ]
 
+# Trusted origins for CSRF protection
 CSRF_TRUSTED_ORIGINS = [
-    'http://localhost:5173',
+    'http://localhost:5173',  # Local development
+    'https://rxjourney.onrender.com',  # Production
 ]
+
 
 CORS_ALLOW_CREDENTIALS = True
 
@@ -137,6 +144,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Directory where uploaded files will be stored
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
